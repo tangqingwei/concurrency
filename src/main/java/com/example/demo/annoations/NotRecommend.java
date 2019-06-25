f40d069840394793fda64815742b2ef631dd51bf
tangqingwei <tangqingwei@sqhtech.com>
2019-06-25 17:30:31 +0800

    annoations

---
+++ src/main/java/com/example/demo/annoations/NotRecommend.java
@@ -0,0 +1,18 @@
+package com.example.demo.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记【线程安全】的类或写法
+ * @author sqhtech
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+	String value() default "";
+}
